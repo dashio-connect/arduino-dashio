2acdde87b89fd4e201399471aca6721e4c1e473d
craigt <craigt543@gmail.com>
2021-08-09 13:11:30 +1200

    DashioTCPshield added

---
+++ src/DashioTCPshield.cpp
@@ -0,0 +1,67 @@
+#include "DashioTCPshield.h"
+
+DashioTCPshield::DashioTCPshield(DashioDevice *_dashioDevice, uint16_t _tcpPort, bool _printMessages) : server(_tcpPort), dashioConnection(TCP_CONN) {
+    dashioDevice = _dashioDevice;
+    printMessages = _printMessages;
+}
+
+void DashioTCPshield::setup(void (*processIncomingMessage)(DashioConnection *connection)) {
+    processTCPmessageCallback = processIncomingMessage;
+}
+
+void DashioTCPshield::sendMessage(const String& message) {
+    if (alreadyConnected) {
+        server.print(message);
+
+        if (printMessages) {
+            Serial.println(F("---- TCP Sent ----"));
+            Serial.println(message);
+            Serial.println();
+        }
+    }
+}
+
+void DashioTCPshield::startupServer(byte mac[]) {
+    Ethernet.begin(mac); // start listening for clients
+    server.begin(); // Open serial communications and wait for port to open:
+    
+    Serial.print("IP address: ");
+    Serial.println(Ethernet.localIP());
+}
+
+void DashioTCPshield::checkForMessage() {
+  // wait for a new client:
+    EthernetClient client = server.available();
+    client.setTimeout(2000);
+
+    if (client) {
+        if (!alreadyConnected) {
+            client.flush();  // clear out the input buffer:
+            Serial.println("Connected");
+            alreadyConnected = true;
+        }
+    
+        while(client.available()) {
+            char data;
+            data = (char)client.read();
+      
+            if (dashioConnection.processChar(data)) {
+                if (printMessages) {
+                    Serial.println(dashioConnection.getReceivedMessageForPrint(dashioDevice->getControlTypeStr(dashioConnection.control)));
+                }
+    
+                switch (dashioConnection.control) {
+                case who:
+                    sendMessage(dashioDevice->getWhoMessage());
+                    break;
+                case connect:
+                    sendMessage(dashioDevice->getConnectMessage());
+                    break;
+                default:
+                    processTCPmessageCallback(&dashioConnection);
+                    break;
+                }
+            }
+        }
+    }
+}
